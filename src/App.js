b50ec3b52610dbd7be5b1536681a9182b94fc879
alex-edub <alex.baroncello@gmail.com>
2024-10-19 21:38:02 -0300

    Alteração

--- src/App.js
+++ src/App.js
@@ -2,24 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import nome from './nome.js';
 import HelloWorld from './Components/HelloWorld.js';
+import SayMyName from './Components/SayMyName.js';
 
 function App() {
-  const name= "Matheus"
-
-  const newName= name.toUpperCase()
-
-  function sum (a,b){
-  return a+b
-  }
-  const url = 'https://via.placeholder.com/150'
   return (
     
     <div className="App">
-      <h2>Alterando o JSX</h2>
-      <p>Olá, {newName}</p>
-      <p>Soma: {sum(1,2)}</p>
-      <img src={url} alt="Minha imagem" />
       <HelloWorld/>
+      <SayMyName nome="Matheus"/>
     </div>
   )
 }
